f3745ee6150a17d1901c902f7bfba5490cb5e1fe
Hamzi-SE <hamzii.se@gmail.com>
2023-04-12 07:43:10 +0500

    home page ui changed and protected routes using middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
 import Button from '@/components/ui/Button';
+import Link from 'next/link';
 
 export default async function Home() {
-	return <Button>Button</Button>;
+	return (
+		<>
+			<div className='flex flex-col items-center justify-center min-h-screen'>
+				<div className='container mx-auto mt-60 text-center flex-grow'>
+					<h1 className='text-4xl font-bold mb-5'>NextChatter</h1>
+
+					<p className='text-gray-600 mb-5'>A simple real-time chat app built using Typescript, Next.js 13 and Upstash.</p>
+
+					<Link href='/login'>
+						<Button size='lg'>Login</Button>
+					</Link>
+				</div>
+
+				<footer className='bg-gray-200 text-center text-sm py-2 w-full'>
+					<p>
+						Made with <span className='text-red-500'>❤</span> by{' '}
+						<a href='https://twitter.com/Hamzii_SE' target='_blank' rel='noreferrer'>
+							<strong>Hamza</strong>
+						</a>
+						{' | '}
+						<a href='https://github.com/Hamzi-SE/NextChatter' target='_blank' rel='noreferrer'>
+							<strong>Source Code</strong>
+						</a>
+					</p>
+
+					<p>
+						&copy; 2023 by{' '}
+						<a href='https://hamzii.me' target='_blank' rel='noreferrer'>
+							<strong>Hamzii.me</strong>{' '}
+						</a>
+						All rights reserved.
+					</p>
+				</footer>
+			</div>
+		</>
+	);
 }
---
+++ src/middleware.ts
@@ -0,0 +1,44 @@
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+	async function middleware(req) {
+		const pathname = req.nextUrl.pathname;
+
+		// Manage route protection
+		const isAuth = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+		const isLoginPage = pathname.startsWith('/login');
+
+		const sensitiveRoutes = ['/dashboard'];
+		const isAccessingSensitiveRoute = sensitiveRoutes.some((route) => pathname.startsWith(route));
+
+		if (isLoginPage) {
+			if (isAuth) {
+				return NextResponse.redirect(new URL('/dashboard', req.url));
+			}
+
+			return NextResponse.next();
+		}
+
+		if (!isAuth && isAccessingSensitiveRoute) {
+			return NextResponse.redirect(new URL('/login', req.url));
+		}
+
+		if (isAuth && pathname === '/') {
+			return NextResponse.redirect(new URL('/dashboard', req.url));
+		}
+	},
+	{
+        // Include callbacks to handle infinite redirects (works with `withAuth` and is a workaround for now, mentioned in the docs)
+		callbacks: {
+			async authorized() {
+				return true;
+			},
+		},
+	}
+);
+
+export const config = {
+	matcher: ['/', '/login', '/dashboard/:path*'],
+};
