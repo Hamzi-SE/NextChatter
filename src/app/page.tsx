8a4b703df9e76611918b0ebb714390f376ffa525
Hamzi-SE <hamzii.se@gmail.com>
2023-04-12 23:17:11 +0500

    style improvements (home page desgin)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 import Button from '@/components/ui/Button';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Home() {
 	return (
 		<>
 			<div className='flex flex-col items-center justify-center min-h-screen'>
-				<div className='container mx-auto mt-60 text-center flex-grow'>
-					<h1 className='text-4xl font-bold mb-5'>NextChatter</h1>
+				<Image src='/cool-background.svg' alt='bg-image' fill style={{ objectFit: 'cover', zIndex: '-1' }} />
+				<div className='container mt-44 text-center flex-grow'>
+					<h1 className='text-6xl font-bold mb-5'>NextChatter</h1>
 
-					<p className='text-gray-600 mb-5'>A simple real-time chat app built using Typescript, Next.js 13 and Upstash.</p>
+					<p className='text-black text-xl font-semibold mb-5'>A simple real-time chat app built using Next.js 13, Typescript and Upstash.</p>
 
 					<Link href='/login'>
-						<Button size='lg'>Login</Button>
+						<Button size='xl' className='text-base'>
+							Login
+						</Button>
 					</Link>
 				</div>
 
-				<footer className='bg-gray-200 text-center text-sm py-2 w-full'>
+				<footer className='text-center text-sm py-2 w-full'>
 					<p>
 						Made with <span className='text-red-500'>❤</span> by{' '}
 						<a href='https://twitter.com/Hamzii_SE' target='_blank' rel='noreferrer'>
